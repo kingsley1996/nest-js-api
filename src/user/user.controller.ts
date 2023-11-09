cef6ded538c478695e76d87f60dd168fb5676499
Linh Dang <linhdang.dev96@gmail.com>
2023-11-09 21:52:22 +0700

    add bookmarks api route and fully test e2e

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, UseGuards, Patch } from '@nestjs/common';
+import { Controller, Body, Get, UseGuards, Patch } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { Request } from 'express';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
+    constructor(private userService: UserService) {}
     @Get('me')
     getMe(
         @GetUser() user: User
@@ -18,7 +21,10 @@ export class UserController {
     }
 
     @Patch()
-    editUser() {
-
+    editUser(
+        @GetUser('id') userId: number,
+        @Body() dto: EditUserDto
+    ) {
+        return this.userService.editUser(userId, dto);
     }
 }
